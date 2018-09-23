58676f416d85b75165133f6323a0bda7f0999d94
wajinosuke <w2physalis+github@gmail.com>
2018-09-24 00:21:39 +0900

    selenium+typescript+ieで簡単なテストが動作

---
+++ src/sample.test.ts
@@ -0,0 +1,14 @@
+import { Builder, By, Key, until } from 'selenium-webdriver';
+
+describe('selenium test', () => {
+    it('open test', (async () => {
+        let driver = await new Builder().forBrowser('internet explorer').build();
+        await driver.manage().window().maximize();
+        // プロトコルがfileだとダメだった。
+        await driver.get('http://127.0.0.1:8080/src/sample.html');
+        const title = await driver.getTitle();
+        expect(title).toBe('サンプルのHTML');
+        const test1 = await driver.findElement(By.id('test1')).getText();
+        expect(test1).toBe('テキスト');
+    }),600000);
+});
